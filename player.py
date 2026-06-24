91f1ef1923cfb1c9d4b40851580a74dc91ac1d76
Chris <chrismtz2003@gmail.com>
2026-06-23 21:35:19 -0400

    Added header with key componets of important details and class details

---
+++ player.py
@@ -0,0 +1,7 @@
+"""
+Program Name: Match Coins Game
+Author: Chris Martinez
+Purpose: This class represents a player with a name, wallet, and a coin object
+Starter Code: N/A
+Date: June 23, 2026
+"""
