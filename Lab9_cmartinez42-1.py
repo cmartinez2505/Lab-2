46e8da77289cf4d1c642ca74012ca62e4199adfe
Chris <chrismtz2003@gmail.com>
2026-06-24 00:58:03 -0400

    Added main header to explain what this file is used for

---
+++ Lab9_cmartinez42-1.py
@@ -0,0 +1,8 @@
+"""
+Program Name: Match Coins Game
+Author: Chris Martinez
+Purpose: This class manages the game itself like the game loop and declaring who the winner is
+Starter Code: N/A
+Date: June 23, 2026
+"""
+
