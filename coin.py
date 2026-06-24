c095154b9fbbd769b8faac06f1e297ee9c24d6b9
Chris <chrismtz2003@gmail.com>
2026-06-23 21:27:18 -0400

    Made the Coin class with a __init__, getter, and toss function

--- coin.py
+++ coin.py
@@ -5,3 +5,18 @@ Purpose: This class represents a single coin that can be flipped.
 Starter Code: N/A
 Date: June 23, 2026
 """
+
+import random
+
+class Coin:
+    def __init__(self):
+        self.__sideup = "Heads"
+
+    def toss(self):
+        if random.randit(0, 1) == 0:
+            self.__sideup = "Heads"
+        else:
+            self.__sideup = "Tails"    
+
+    def get_sideup(self):
+        return self.__sideup
