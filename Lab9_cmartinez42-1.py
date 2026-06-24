89680bef281e1d986d0fdd2537091821e5d2f54b
Chris <chrismtz2003@gmail.com>
2026-06-24 02:11:04 -0400

    Added my main function which includes the main componets of the program

--- Lab9_cmartinez42-1.py
+++ Lab9_cmartinez42-1.py
@@ -6,3 +6,43 @@ Starter Code: N/A
 Date: June 23, 2026
 """
 
+from player import Player
+
+def main():
+    player1 = Player("Player 1")
+    player2 = Player("Player 2")
+
+    user_input = input("Do you want to toss the coins? (y/n): ")
+
+    while user_input == "y" or user_input == "Y":
+
+        player1.toss_coin()
+        player2.toss_coin()
+
+        side1 = player1.get_coin_side()
+        side2 = player2.get_coin_side()
+
+        print("Tossing...")
+
+        print("Player 1 tossed " + side1)
+        print("Player 2 tossed " + side2)
+
+        if side1 == side2:
+            player1.win_coin()
+            player2.lose_coin()
+            print("Its a Match! Player 1 wins a coin.")
+        else:
+            player1.lose_coin()
+            player2.win_coin()
+            print("No Match! Player 2 wins a coin.")
+
+            print("Player 1 has " + player1.get_wallet + " coins.")
+            print("Player 2 has " + player2.get_wallet + " coins.")
+
+            user_input = input("Do you want to toss the coins? (y/n): ")
+
+ 
+            
+
+
+
